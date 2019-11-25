633dd83457e02e5eca75c9c64f7b4f1126254193
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2019-11-25 15:42:27 -0800

    completed saturdays lab...i think

--- fun_land.rb
+++ fun_land.rb
@@ -2,7 +2,12 @@ require 'faked'
 
 File.open('rewards_seed.txt', 'w+') do |file|
 	100.times do |i|
-		file.write ('rewards_seed.txt', 'INSERT INTO rewards(item_name, tokens, available) VALUES (Faker::Creature::Animal.name, Faker::Number.between(from: 5, to: 5000), [true, true, true, false].sample;')
+		file.write ('rewards_seed.txt', 'INSERT INTO rewards(item_name, tokens, available) VALUES (Faker::Creature::Animal.name, Random.rand(), [true, true, true, false].sample;')
+	end
+end 
 
+File.open('guests_seed.txt', 'w+') do |file|
+	300.times do |i|
+		file.write( Faker::Name.name, Random.rand(10000) )
 	end
 end
